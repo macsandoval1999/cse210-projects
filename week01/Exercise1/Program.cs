90e97322f572ee00231fd9898a1324f48ca1f7b7
macsandoval1999 <msandoval2@byupathway.edu>
2025-05-21 18:46:40 -0700

    Completed Week 1 Excercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Prompt the user for a first name from the console
+        Console.Write("What is your first name? ");
+
+        // Store the first name in a variable
+        string firstName = Console.ReadLine();
+
+        // Prompt the user for a last name from the console
+        Console.Write("What is your last name? ");
+
+        // Store the last name in a variable
+        string lastName = Console.ReadLine();
+
+        // print a blank line
+        Console.WriteLine("");
+
+        // Print the first name and last name in the format "Your name is <lastName>, <firstName> <lastName>"
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
